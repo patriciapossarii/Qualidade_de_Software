83ceffedc605079606b68d08b57a23baa86e0fa7
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-05-10 20:16:25 -0300

    testando variaveis

---
+++ lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -0,0 +1,12 @@
+package br.com.lojinha;
+
+public class LojinhaApp {
+    public static void main(String[] args) {
+        System.out.println("Testes e Qualidade de Software");
+        String joias = "anel";
+        int quantidadeJoia = 2;
+        double pesoDaJoia = 2.5;
+        boolean verdadeiro = true;
+
+    }
+}
