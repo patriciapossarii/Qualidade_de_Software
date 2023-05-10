2acf1efb0e7c6698d0783865ed29016b70b86aa4
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-05-10 20:30:48 -0300

    criado a classe produto

--- lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
+++ lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,12 +1,18 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojo.Produto;
+
 public class LojinhaApp {
     public static void main(String[] args) {
         System.out.println("Testes e Qualidade de Software");
-        String joias = "anel";
-        int quantidadeJoia = 2;
-        double pesoDaJoia = 2.5;
-        boolean verdadeiro = true;
+        Produto meuProduto = new Produto();
+        meuProduto.nome = "Pay Station 4";
+        meuProduto.valor = 4999.99;
+        meuProduto.marca = "Sony";
+        meuProduto.tamanho = "Medio";
+        meuProduto.itensInclusos = "2 Controles e 3 Jogos";
+        System.out.println(meuProduto.nome);
+
 
     }
 }
---
+++ lojinha/src/main/java/br/com/lojinha/pojo/Produto.java
@@ -0,0 +1,9 @@
+package br.com.lojinha.pojo;
+
+public class Produto {
+    public String nome;
+    public String marca;
+    public double valor;
+    public String itensInclusos;
+    public String tamanho;
+}
